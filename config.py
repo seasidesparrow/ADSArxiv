d3b3a9c44cc2419c3ce84933169312144318c407
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2018-02-15 08:21:26 -0500

    modified:   config.py

--- config.py
+++ config.py
@@ -10,14 +10,14 @@ ARCHIVE_ABS_DIR = INCOMING_ABS_DIR + '/oai/arXiv.org'
 ## ================= celery/rabbitmq rules============== #
 ## ##################################################### #
 #
-#ACKS_LATE=True
-#PREFETCH_MULTIPLIER=1
-#CELERYD_TASK_SOFT_TIME_LIMIT = 5
+ACKS_LATE=True
+PREFETCH_MULTIPLIER=1
+CELERYD_TASK_SOFT_TIME_LIMIT = 5
 
 CELERY_DEFAULT_EXCHANGE = 'arxiv_pipeline'
 CELERY_DEFAULT_EXCHANGE_TYPE = "topic"
 
 CELERY_INCLUDE = []
-CELERY_BROKER = 'pyamqp://user:password@localhost:5682/arxiv_pipeline'
-OUTPUT_CELERY_BROKER = 'pyamqp://user:password@localhost:5682/master_pipeline'
-OUTPUT_TASKNAME = 'tasks.task_update_record'
+CELERY_BROKER = 'pyamqp://guest:guest@localhost:5682/arxiv_pipeline'
+OUTPUT_CELERY_BROKER = 'pyamqp://guest:guest@localhost:5682/master_pipeline'
+OUTPUT_TASKNAME = 'adi.tasks.task_update_record'
