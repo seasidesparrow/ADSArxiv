af266255a12aa967a09315fdc6dafc19db75035e
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2018-02-21 14:36:34 -0500

    new file:   .travis.yml  	new file:   ADSArxiv/tests/__init__.py  	modified:   config.py  	new file:   pytest.ini

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+LOGGING_LEVEL = 'DEBUG'
+
+## ================ ArXiv data location ================ #
+## ##################################################### #
+# Where the worker is going to look for incoming ArXiv
+# submissions: ARCHIVE_ABS_DIR
+
 INCOMING_ABS_DIR = '/proj/ads/abstracts/sources/ArXiv'
 
 UPDATE_AGENT_DIR = INCOMING_ABS_DIR + '/UpdateAgent'
@@ -10,14 +17,15 @@ ARCHIVE_ABS_DIR = INCOMING_ABS_DIR + '/oai/arXiv.org'
 ## ================= celery/rabbitmq rules============== #
 ## ##################################################### #
 #
+
+CELERY_INCLUDE = ['ADSArxiv.tasks']
 ACKS_LATE=True
 PREFETCH_MULTIPLIER=1
-CELERYD_TASK_SOFT_TIME_LIMIT = 5
+CELERYD_TASK_SOFT_TIME_LIMIT = 300
+CELERY_BROKER = 'pyamqp://'
 
 CELERY_DEFAULT_EXCHANGE = 'arxiv_pipeline'
 CELERY_DEFAULT_EXCHANGE_TYPE = "topic"
 
-CELERY_INCLUDE = []
-CELERY_BROKER = 'pyamqp://guest:guest@localhost:5682/arxiv_pipeline'
 OUTPUT_CELERY_BROKER = 'pyamqp://guest:guest@localhost:5682/master_pipeline'
-OUTPUT_TASKNAME = 'adi.tasks.task_update_record'
+OUTPUT_TASKNAME = 'adsmp.tasks.task_update_record'
